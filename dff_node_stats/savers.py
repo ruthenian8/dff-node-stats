8ca0f8e0dd403e5a3ae0dc1b5cd6442bc5189871
ruthenian8 <ruthenian8@gmail.com>
2021-12-16 01:15:49 +0300

    rework saver, hardcode table name; presumably bypassed the table creation troubles

--- dff_node_stats/savers.py
+++ dff_node_stats/savers.py
@@ -2,10 +2,34 @@
 from typing import Dict, List, Union, Optional, Protocol, runtime_checkable
 import pathlib
 import pandas as pd
-
+import csv
 
 @runtime_checkable
 class Saver(Protocol):
+    _saver_mapping = {}
+
+    def __init_subclass__(cls, _id: str, **kwargs) -> None:
+        super().__init_subclass__(**kwargs)
+        cls._saver_mapping[_id] = cls
+
+    def __new__(cls, path: str):
+        triple = path.partition("://")
+        if not all(triple):
+            raise ValueError(
+                """Saver should be initialized with either:
+                csv://path_to_file 
+                or
+                postgresql://sqlalchemy_engine_params
+                clickhouse://sqlalchemy_engine_params
+                """
+            )
+        _id = triple[0]
+        subclass = cls._saver_mapping[_id]
+        obj = object.__new__(subclass)
+        obj.path = path
+        return obj
+
+
     def save(self, dfs: List[pd.DataFrame], **kwargs) -> None:
         """
         Save the data to a database or a file
@@ -20,38 +44,48 @@ class Saver(Protocol):
         raise NotImplementedError
 
 
-class CsvSaver:
-    def __init__(self, csv_file: str) -> None:
-        self.csv_file = pathlib.Path(csv_file)
+class CsvSaver(Saver, _id="csv"):
+    def __init__(
+        self, 
+        path: str, 
+    ) -> None:
+        if hasattr(self, "path"):
+            path = self.path.partition("://")[2]
+        self.path = pathlib.Path(path)
 
     def save(self, dfs: List[pd.DataFrame], **kwargs) -> None:
         column_types: Optional[Dict[str, str]] = kwargs.get("column_types")
         parse_dates: Optional[List[str]] = kwargs.get("parse_dates", False)
         saved_df = (
             self.load(column_types=column_types, parse_dates=parse_dates)
-            if self.csv_file.exists()
+            if self.path.exists()
             else pd.DataFrame()
         )
-        pd.concat([saved_df] + dfs).to_csv(self.csv_file, index=False)
+        pd.concat([saved_df] + dfs).to_csv(self.path, index=False)
 
     def load(self, **kwargs) -> pd.DataFrame:
         column_types: Optional[Dict[str, str]] = kwargs.get("column_types")
         parse_dates: Optional[List[str]] = kwargs.get("parse_dates", False)
-        return pd.read_csv(self.csv_file, dtype=column_types, parse_dates=parse_dates)
+        return pd.read_csv(self.path, dtype=column_types, parse_dates=parse_dates)
 
 
-class PGSaver:
-    def __init__(self, table: str, engine_params: dict) -> None:
+class PGSaver(Saver, _id="postgresql"):
+    def __init__(
+        self, 
+        path: str
+    ) -> None:
         """
-        :param table: table name
         :param engine_params: dict of format {user, password, host, port, db}
         """
         import sqlalchemy as sa
-
+        if not hasattr(self, "path"):
+            self.path = path
+        self.table = "dff_stats"
         self.json_type = sa.dialects.postgresql.JSONB
-        self.table = table
         self.engine = sa.create_engine(
-            "postgresql://{user}:{password}@{host}:{port}/{db}".format(**engine_params)
+            self.path,
+            echo=True,
+            echo_pool='debug'            
         )
 
     def save(self, dfs: List[pd.DataFrame], **kwargs) -> None:
@@ -68,25 +102,73 @@ class PGSaver:
         return pd.read_sql(self.table, self.engine, parse_dates=parse_dates)
 
 
-class CHSaver:
-    def __init__(self, table: str, engine_params: dict) -> None:
+class CHSaver(Saver, _id="clickhouse"):
+    def __init__(
+        self, 
+        path: str
+    ) -> None:
         """
         :param table: table name
         :param engine_params: dict of format {user, password, host, port, db}
         """
         import sqlalchemy as sa
-
+        if not hasattr(self, "path"):
+            self.path = path
+        self.table = "dff_stats"
         self.engine = sa.create_engine(
-            "clickhouse://{user}:{password}@{host}:{port}/{db}".format(**engine_params)
+            self.path,
+            echo=True,
+            echo_pool='debug'
         )
-        self.table = table
 
     def save(self, dfs: List[pd.DataFrame], **kwargs) -> None:
         column_types: Optional[Dict[str, str]] = kwargs.get("column_types")
+        
+        if not self.engine.dialect.has_table(self.engine, self.table):
+            data_model = self.create_clickhouse_table(column_types)
+            with self.engine.connect() as conn:
+                conn.connection.create_table(data_model)
+
         pd.concat(dfs).to_sql(
-            name=self.table, con=self.engine, dtype=column_types, if_exists="append"
+            name=self.table,
+            con=self.engine,
+            if_exists="append",
+            index=False
         )
 
     def load(self, **kwargs) -> pd.DataFrame:
         parse_dates: Union[List[str], bool] = kwargs.get("parse_dates", False)
         return pd.read_sql(self.table, self.engine, parse_dates=parse_dates)
+
+    @staticmethod
+    def create_clickhouse_table(column_types: Dict[str, str]):
+        import infi.clickhouse_orm as orm
+        model_namespace = {
+            "engine": orm.engines.Memory()
+        }
+        ch_mapping = {
+            'object': orm.fields.StringField,
+            'uint64': orm.fields.UInt64Field,
+            'uint32': orm.fields.UInt32Field,
+            'uint16': orm.fields.UInt16Field,
+            'uint8': orm.fields.UInt8Field,
+            'bool': orm.fields.UInt8Field,
+            'float64': orm.fields.Float64Field,
+            'float32': orm.fields.Float32Field,
+            'int64': orm.fields.Int64Field,
+            'int32': orm.fields.Int32Field,
+            'int16': orm.fields.Int16Field,
+            'int8': orm.fields.Int8Field,
+            'datetime64[D]': orm.fields.DateField,
+            'datetime64[ns]': orm.fields.DateTimeField
+        }
+        for column, _type in column_types.items():
+            model_namespace.update(
+                {column: ch_mapping[_type]()}
+            )
+        dff_stats = type(
+            "dff_stats",
+            (orm.models.Model,),
+            model_namespace
+        )
+        return dff_stats
