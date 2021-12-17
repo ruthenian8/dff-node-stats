2bc23052f82c9bdfcee6b9badd9013918e703377
ruthenian8 <ruthenian8@gmail.com>
2021-12-17 20:42:13 +0300

    got postgresql to work

--- dff_node_stats/savers.py
+++ dff_node_stats/savers.py
@@ -66,7 +66,7 @@ class CsvSaver(Saver, _id="csv"):
     def save(self, dfs: List[pd.DataFrame], **kwargs) -> None:
         column_types: Optional[Dict[str, str]] = kwargs.get("column_types")
         parse_dates: Optional[List[str]] = kwargs.get("parse_dates", False)
-        
+
         for key in parse_dates:
             if key in column_types:
                 column_types.pop(key)
@@ -89,31 +89,40 @@ class PGSaver(Saver, _id="postgresql"):
 
         if not hasattr(self, "path"):
             self.path: str = path
-        self.schema: str = self.path[self.path.rfind("/") + 1:]
+        self.schema: str = self.path[self.path.rfind("/") + 1 :]
         self.table: str = "dff_stats"
-        self.json_type = sa.dialects.postgresql.JSONB
-        self.engine = None
         self.engine_factory = sa.create_engine
+        self.engine = None
 
     def save(self, dfs: List[pd.DataFrame], **kwargs) -> None:
         column_types: Optional[Dict[str, str]] = kwargs.get("column_types")
-        for key in column_types:
-            if column_types[key] == "object":
-                column_types[key] = self.json_type
-        # create engine on the first call of the method        
+        # for key in column_types:
+        #     if column_types[key] == "object":
+        #         column_types[key] = PGSaver.sa.dialects.postgresql.JSONB
+        # create engine on the first call of the method
         if self.engine is None:
-            self.engine = self.engine_factory(self.path, echo=True, echo_pool="debug")
-
-        pd.concat(dfs).to_sql(
-            name=self.table, con=self.engine, dtype=column_types, if_exists="append"
-        )
+            self.engine = self.engine_factory(
+                self.path,
+                # echo=True,
+                # echo_pool="debug"
+            )
+        pd.concat(dfs).to_sql(name=self.table, con=self.engine, if_exists="append")
+        self.engine = None
 
     def load(self, **kwargs) -> pd.DataFrame:
         parse_dates: Union[List[str], bool] = kwargs.get("parse_dates", False)
         # create engine on the first call of the method
         if self.engine is None:
-            self.engine = self.engine_factory(self.path, echo=True, echo_pool="debug")
-        return pd.read_sql(self.table, self.engine, parse_dates=parse_dates)
+            self.engine = self.engine_factory(
+                self.path,
+                # echo=True,
+                # echo_pool="debug"
+            )
+        df = pd.read_sql_table(
+            table_name=self.table, con=self.engine, parse_dates=parse_dates
+        )
+        self.engine = None
+        return df
 
 
 class CHSaver(Saver, _id="clickhouse"):
@@ -122,9 +131,9 @@ class CHSaver(Saver, _id="clickhouse"):
 
         if not hasattr(self, "path"):
             self.path = path
-        self.schema: str = self.path[self.path.rfind("/") + 1:]
+        self.schema: str = self.path[self.path.rfind("/") + 1 :]
         self.table: str = "dff_stats"
-        # TODO: engine can't be assigned on init, 
+        # TODO: engine can't be assigned on init,
         # because the object instance should be pickled
         # during the actor validation phase, whereas an engine
         # contains threads. It means we should create an engine every time,
@@ -137,7 +146,11 @@ class CHSaver(Saver, _id="clickhouse"):
         parse_dates: List[str] = kwargs.get("parse_dates")
         # create engine when the method is called
         if self.engine is None:
-            self.engine = self.engine_factory(self.path, echo=True, echo_pool="debug")
+            self.engine = self.engine_factory(
+                self.path,
+                # echo=True,
+                # echo_pool="debug"
+            )
         # table should be created preemptively, since
         # sqlalchemy-clickhouse does not implement table creation
         if not self.engine.dialect.has_table(self.engine, self.table):
@@ -148,7 +161,7 @@ class CHSaver(Saver, _id="clickhouse"):
         df = pd.concat(dfs)
         # Clickhouse requires Datetime to be rounded to seconds
         for column in parse_dates:
-            df[column] = df[column].dt.round('S')
+            df[column] = df[column].dt.round("S")
 
         # df.append(pd.Series(), ignore_index=True).to_sql(
         df.to_sql(
@@ -156,15 +169,19 @@ class CHSaver(Saver, _id="clickhouse"):
             con=self.engine,
             if_exists="append",
             index=False,
-            method=self.clickhouse_insert
+            method=self.clickhouse_insert,
         )
-        #remove the engine
+        # remove the engine
         self.engine = None
 
     def load(self, **kwargs) -> pd.DataFrame:
         parse_dates: Union[List[str], bool] = kwargs.get("parse_dates", False)
         # create engine on the first call of the method
-        self.engine = self.engine_factory(self.path, echo=True, echo_pool="debug")
+        self.engine = self.engine_factory(
+            self.path,
+            # echo=True,
+            # echo_pool="debug"
+        )
         df = pd.read_sql(
             sql=f"SELECT * FROM {self.schema}.{self.table}",
             con=self.engine,
@@ -181,7 +198,7 @@ class CHSaver(Saver, _id="clickhouse"):
         model_namespace = {"engine": orm.engines.Memory()}
         ch_mapping = {
             "object": orm.fields.StringField,
-            "str":orm.fields.StringField,
+            "str": orm.fields.StringField,
             "uint64": orm.fields.UInt64Field,
             "uint32": orm.fields.UInt32Field,
             "uint16": orm.fields.UInt16Field,
@@ -202,20 +219,15 @@ class CHSaver(Saver, _id="clickhouse"):
         return dff_stats
 
     @staticmethod
-    def clickhouse_insert(
-        table,
-        conn,
-        keys,
-        data_iter
-    ) -> None:
+    def clickhouse_insert(table, conn, keys, data_iter) -> None:
         table_name = table.name
         dbapi_conn = conn.connection
         cur = dbapi_conn.cursor()
-        entries =  ", ".join(
+        entries = ", ".join(
             [
-                "(" + ", ".join(
-                    ["'" + str(item).replace("'", "\\'") + "'" for item in row]
-                ) + ")" 
+                "("
+                + ", ".join(["'" + str(item).replace("'", "\\'") + "'" for item in row])
+                + ")"
                 for row in data_iter
             ]
         )
@@ -224,6 +236,7 @@ class CHSaver(Saver, _id="clickhouse"):
             cur.execute(sql)
         except RuntimeError:
             return
+
 
 # class InfiSaver(Saver, _id="clickhouse"):
 #     """Alternative clickhouse saver"""
@@ -234,7 +247,7 @@ class CHSaver(Saver, _id="clickhouse"):
 #         return
 
 #     def save(self, dfs: List[pd.DataFrame], **kwargs) -> None:
-        
+
 #         return
 
 #     def load(self, **kwargs) -> pd.DataFrame:
