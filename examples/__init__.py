173f284737f092cdaa3bbe38c310b0df82bcbb2d
ruthenian8 <ruthenian8@gmail.com>
2022-09-30 18:34:24 +0300

    apply formatting

--- examples/__init__.py
+++
@@ -1,3 +0,0 @@
-# TODO: add examples with usage our pool and with custom pool
-# TODO: get naming for example from the pipeline (we get some feature and describe it in two examples)
-# TODO: naming basic and advanced
